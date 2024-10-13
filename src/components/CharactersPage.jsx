4c7cb4bf291f5890b93604f22164728f3f34fa7b
Azrane <azrane@gmail.com>
2024-10-13 00:23:21 -0500

    Tabs are cool.

--- src/components/CharactersPage.jsx
+++ src/components/CharactersPage.jsx
@@ -5,66 +5,66 @@ import TypeFilter from './TypeFilter';
 import "./CharactersPage.css";
 
 function CharactersPage(props) {
-  const [searchText, setSearchText] = useState('');
-  const [elementFilter, setElementFilter] = useState(undefined);
-  const [weaponFilter, setWeaponFilter] = useState(undefined);
+	const [searchText, setSearchText] = useState('');
+	const [elementFilter, setElementFilter] = useState(undefined);
+	const [weaponFilter, setWeaponFilter] = useState(undefined);
 	const [regionFilter, setRegionFilter] = useState(undefined);
 	const [versionFilter, setVersionFilter] = useState(undefined);
 
-  return (
-    <>
-      <div className="filters">
-        <div className="search-filter">
-          <input
-            type="text"
-            value={searchText}
-            placeholder="Search for a character..."
-            onChange={(e) => setSearchText(e.target.value)} />
-        </div>
-        <TypeFilter type="element" checked={elementFilter} selectFilter={setElementFilter} />
-        <TypeFilter type="weapon" checked={weaponFilter} selectFilter={setWeaponFilter} />
+	return (
+		<>
+			<div className="filters">
+				<div className="search-filter">
+					<input
+						type="text"
+						value={searchText}
+						placeholder="Search for a character..."
+						onChange={(e) => setSearchText(e.target.value)} />
+				</div>
+				<TypeFilter type="element" checked={elementFilter} selectFilter={setElementFilter} />
+				<TypeFilter type="weapon" checked={weaponFilter} selectFilter={setWeaponFilter} />
 				<TypeFilter type="region" checked={regionFilter} selectFilter={setRegionFilter} />
 				<TypeFilter type="version" checked={versionFilter} selectFilter={setVersionFilter} />
-      </div>
-      <div className="character-list">
-        {
-          Object.entries(props.characters)
-            .filter(([key, value]) => searchText === undefined || key.includes(searchText.toLowerCase()))
-            .filter(([key, value]) => elementFilter === undefined || elementFilter === value.type || value.type === 'flex')
-            .filter(([key, value]) => weaponFilter === undefined || weaponFilter === value.weapon)
+			</div>
+			<div className="character-list">
+				{
+					Object.entries(props.characters)
+						.filter(([key, value]) => searchText === undefined || key.includes(searchText.toLowerCase()))
+						.filter(([key, value]) => elementFilter === undefined || elementFilter === value.type || value.type === 'flex')
+						.filter(([key, value]) => weaponFilter === undefined || weaponFilter === value.weapon)
 						.filter(([key, value]) => regionFilter === undefined || regionFilter === value.region)
 						.filter(([key, value]) => versionFilter === undefined || versionFilter === value.releaseversion)
-            .map(([key, value]) => (
-              <div key={key} className="character-portrait">
-                <button className="character-toggle">
-                  <img
-                    className={"character-icon" + (props.selected(key) ? "" : " inactive")}
-                    src={process.env.PUBLIC_URL + "/images/characters/" + value.name + ".png"}
-                    alt={value.name}
-                    onClick={(e) => props.toggleCharacter(key, e)}
-                    loading="lazy" />
-                </button>
-                {
-                  key !== 'traveler' &&
-                  <img
-                    className={"character-type" + (props.selected(key) ? "" : " inactive")}
-                    src={process.env.PUBLIC_URL + "/images/" + value.type + ".png"}
-                    alt={value.type}
-                    loading="lazy" />
-                }
-                <StarLevel
-                  name={key}
-                  level={props.getCharacterStage(key)}
-                  disabled={!props.selected(key)}
-                  setStage={props.setCharacterStage}
-                />
-                <NavLink to={`/characters/${key}`} className="character-name">{value.name}</NavLink>
-              </div>
-            ))
-        }
-      </div>
-    </>
-  )
+						.map(([key, value]) => (
+							<div key={key} className="character-portrait">
+								<button className="character-toggle">
+									<img
+										className={"character-icon" + (props.selected(key) ? "" : " inactive")}
+										src={process.env.PUBLIC_URL + "/images/characters/" + value.name + ".png"}
+										alt={value.name}
+										onClick={(e) => props.toggleCharacter(key, e)}
+										loading="lazy" />
+								</button>
+								{
+									key !== 'traveler' &&
+									<img
+										className={"character-type" + (props.selected(key) ? "" : " inactive")}
+										src={process.env.PUBLIC_URL + "/images/" + value.type + ".png"}
+										alt={value.type}
+										loading="lazy" />
+								}
+								<StarLevel
+									name={key}
+									level={props.getCharacterStage(key)}
+									disabled={!props.selected(key)}
+									setStage={props.setCharacterStage}
+								/>
+								<NavLink to={`/characters/${key}`} className="character-name">{value.name}</NavLink>
+							</div>
+						))
+				}
+			</div>
+		</>
+	)
 }
 
 export default CharactersPage
